4110ab5f6d92a75c43cf96394737b0500e614871
“kieuvu” <kieuvuvuvu@gmail.com>
2022-04-21 15:45:24 +0700

    sth new

---
+++ Prepare/Algorithms/2.Implementation/11.DrawingBook.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+  const frontFlip = Math.floor(p / 2);
+  const backFlip = Math.floor((n / 2) - frontFlip);
+  return Math.min(frontFlip, backFlip);
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const p = parseInt(readLine().trim(), 10);
+
+  const result = pageCount(n, p);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
