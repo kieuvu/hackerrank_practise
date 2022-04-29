73961390789be2a7be016992742d11751e020ba2
“kieuvu” <kieuvuvuvu@gmail.com>
2022-04-29 13:32:23 +0700

    up

---
+++ Prepare/Algorithms/2.Implementation/20.FindDigits.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'findDigits' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER n as parameter.
+ */
+
+function findDigits(n) {
+  let count = 0;
+
+  const digits = n.toString().split("");
+
+  digits.forEach(d => {
+    if (n % parseInt(d) == 0) count++;
+  });
+
+  return count;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const t = parseInt(readLine().trim(), 10);
+
+  for (let tItr = 0; tItr < t; tItr++) {
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = findDigits(n);
+
+    ws.write(result + '\n');
+  }
+
+  ws.end();
+}
---
+++ Prepare/Algorithms/2.Implementation/21.EqualizeTheArray.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'equalizeArray' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function equalizeArray(arr) {
+  let deleteCount = 0;
+
+  const uniqueElm = [...new Set(arr)];
+
+  let result = {
+    number: uniqueElm[0],
+    count: 0
+  };
+
+  uniqueElm.forEach(elm => {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (elm == arr[i]) {
+        count++;
+      }
+    }
+    if (count > result.count) {
+      result.count = count;
+      result.number = elm;
+    }
+  });
+
+  return arr.length - result.count;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+  const result = equalizeArray(arr);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
